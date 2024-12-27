ed1642464865afc7af77b370528dc8c90a834699
Jeremy <jeremy.farrugia.20@um.edu.mt>
2024-12-27 19:58:15 +0100

    added user accounts and some basic TTS (will be changed, current TTS exists for testing only)

--- Flask/flask-app.py
+++ Flask/flask-app.py
@@ -1,14 +1,90 @@
-from flask import Flask, render_template, send_from_directory, abort, request, jsonify
+from flask import Flask, render_template, send_from_directory, abort, request, jsonify, send_file, session, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 import os, json
+import torchaudio
+import numpy as np
+import soundfile as sf
 from typing import Union
+
+import sqlite3
+
+
+"""
+TODO: 
+users
+- modal popups/updates instead of alerts
+- logout
+- profile?
+
+chat persistance
+- store chat history
+- delete chat?
+
+share chat?
+public chat page?
+
+audio generation
+- TTS & sound effects
+
+story creation
+
+images
+"""
+
+AUTHENTICATION_TOKEN = 'a123' # Authentication token for the user
+
+from TTS.api import TTS
+tts = TTS("tts_models/en/ljspeech/tacotron2-DDC")
+
+SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
+STATIC_DIR = os.path.join(SCRIPT_DIR, 'static')
+USERDATA_DIR = os.path.join(STATIC_DIR, 'userdata')
+AUDIO_DIR = os.path.join(STATIC_DIR, 'audio')
+
+#-------------------------------------------------------Server Setup-------------------------------------------------------#
 
 app = Flask(__name__)
 
 app.static_folder = 'static' # Set the static folder to the 'static' folder in the current directory
 app.template_folder = 'templates' # Set the template folder to the 'templates' folder in the current directory
 
+app.secret_key = os.urandom(24) # Set the secret key for the session
 
-AUTHENTICATION_TOKEN = 'a123' # Authentication token for the user
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(USERDATA_DIR, "users.db")}' # Set the database URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # Disable modification tracking
+db = SQLAlchemy(app) # Create a database object
+bcrypt = Bcrypt(app) # Create a bcrypt object for password hashing
+
+# User Model
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+def log_to_console(message: str, tag: Union[str | None] = None, spacing: int = 0) -> None:
+    """Log a message to the console"""
+    if tag is None:
+        tag = "ROOT"
+    spacing = max(0, spacing)
+    spacing = min(5, spacing)
+    space = "\n" * spacing
+    print(f"{space}SERVER LOG [{tag}] {message}{space}")
+
+def create_tables(db: SQLAlchemy) -> None:
+    """Create the tables in the database"""
+    log_to_console("Creating tables in the database", tag="DATABASE", spacing=1)
+    db.create_all()
+
+def init_db() -> None:
+    """Initialise the database"""
+    if not os.path.exists(os.path.join(USERDATA_DIR, 'users.db')):
+        # This is done after running the app because the db requires the app to be running
+        log_to_console("User Database not found.", tag="DATABASE", spacing=1)
+        with app.app_context():
+            create_tables(db)
+    else:
+        log_to_console("User Database found.", tag="DATABASE", spacing=1)
 
 #-----------------------------------------------------Helper Functions-----------------------------------------------------#
 
@@ -24,14 +100,19 @@ def authenticate_and_send_file(token: str, file_path: str):
     else:
         abort(403)
 
-def log_to_console(message: str, tag: Union[str | None] = None, spacing: int = 0):
-    """Log a message to the console"""
-    if tag is None:
-        tag = "ROOT"
-    spacing = max(0, spacing)
-    spacing = min(5, spacing)
-    space = "\n" * spacing
-    print(f"{space}SERVER LOG [{tag}] {message}{space}")
+def create_user(user_name: str, password: str) -> None:
+    """
+    Create a new user with the given username and password
+    """
+    user_dir = os.path.join(USERDATA_DIR, user_name)
+    os.makedirs(user_dir)
+    os.makedirs(os.path.join(user_dir, 'chat history'))
+
+def loginSuccess(username: str) -> None:
+    """
+    Create a new user with the given username and password
+    """
+    session['username'] = username
 
 #-----------------------------------------------------Routes-----------------------------------------------------#
 
@@ -63,6 +144,156 @@ def prompt():
             'success': False,
             'error': str(e)
         })
+    
+@app.route('/generate-audio', methods=['POST'])
+def generate_audio():
+    try:
+        # Get user input (e.g., text or story context)
+        request_data = {
+            'text': request.json.get('text', ''),
+            'username': request.json.get('username', ''),
+            'chatID': request.json.get('chatID', ''),
+            'index': request.json.get('index', '')
+        }
+
+        log_to_console(f"Received request to generate audio: {request_data}", tag="GENERATE-AUDIO", spacing=1)
+
+        if not request_data['text']:
+            log_to_console("No text provided", tag="GENERATE-AUDIO", spacing=1)
+            return jsonify({'error': 'No text provided'}), 400
+        
+        # Check if the request_data['username'] exists
+        if not os.path.exists(os.path.join(USERDATA_DIR, request_data['username'])):
+            log_to_console(f"Usernam '{request_data['username']}' not found", tag="GENERATE-AUDIO", spacing=1)
+            return jsonify({'error': 'User not found'}), 404
+
+        # Generate audio using WaveNet or equivalent model
+        output_path = os.path.join(USERDATA_DIR, request_data['username'], 'chat history', str(request_data['chatID']), 'audio', f'{request_data["index"]}.wav')
+
+        # Check if the audio file already exists
+        if os.path.exists(output_path):
+            log_to_console(f"Audio file for message {request_data['index']} in chat {request_data['chatID']} already exists", tag="GENERATE-AUDIO", spacing=1)
+            return send_file(output_path, mimetype='audio/wav')
+
+        tts.tts_to_file(request_data['text'], file_path=output_path)
+
+        # Return the audio file to the client
+        return send_file(output_path, mimetype='audio/wav')
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+    
+@app.route('/chat-id', methods=['POST'])
+def get_chat_id():
+    current_user = request.json.get('username', None)
+
+    log_to_console(f"Received request for chat ID from {current_user}", tag="CHAT-ID", spacing=1)
+
+    if current_user is None:
+        log_to_console("No user provided", tag="CHAT-ID", spacing=1)
+        return jsonify({'error': 'No user provided'}), 400
+
+    users = os.listdir(USERDATA_DIR)
+
+    if current_user not in users:
+        log_to_console(f"User {current_user} not found", tag="CHAT-ID", spacing=1)
+        return jsonify({'error': 'User not found'}), 404
+    
+    # Get the chat history of the user
+    chat_history = os.listdir(os.path.join(USERDATA_DIR, current_user, 'chat history'))
+
+    if len(chat_history) == 0:
+        log_to_console(f"No chat history found for {current_user}, starting new chat", tag="CHAT-ID", spacing=1)
+        chat_id = 1
+        chat_dir = os.path.join(USERDATA_DIR, current_user, 'chat history', str(chat_id))
+        # Create chat files
+        os.makedirs(chat_dir)
+        os.makedirs(os.path.join(chat_dir, 'audio'))
+        os.makedirs(os.path.join(chat_dir, 'chat'))
+        # Create messages json
+        with open(os.path.join(chat_dir, 'chat', 'messages.json'), 'w') as f:
+            json.dump([], f)
+        os.makedirs(os.path.join(chat_dir, 'images'))
+    else:
+        # Check if most recent chat is empty
+        most_recent_chat = chat_history[-1]
+        chat_dir = os.path.join(USERDATA_DIR, current_user, 'chat history', most_recent_chat)
+        # Load messages
+        with open(os.path.join(chat_dir, 'chat', 'messages.json'), 'r') as f:
+            messages = json.load(f)
+        if len(messages) == 0:
+            # Use the most recent chat
+            chat_id = int(most_recent_chat)
+        else:
+            # Create a new chat
+            chat_id = len(chat_history) + 1
+
+            chat_dir = os.path.join(USERDATA_DIR, current_user, 'chat history', str(chat_id))
+
+            # Create chat files
+            os.makedirs(chat_dir)
+            os.makedirs(os.path.join(chat_dir, 'audio'))
+            os.makedirs(os.path.join(chat_dir, 'chat'))
+            os.makedirs(os.path.join(chat_dir, 'images'))
+
+    log_to_console(f"Chat ID for {current_user}: {chat_id}", tag="CHAT-ID", spacing=1)
+        
+    return jsonify({'chatID': chat_id}), 200
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.json
+    username = data.get('username')
+    password = data.get('password')
+
+    log_to_console(f"Received login request for user: {username}", tag="LOGIN", spacing=1)
+
+    if not username or not password:
+        return jsonify({'error': 'Missing username or password'}), 400
+
+    user = User.query.filter_by(username=username).first()
+
+    if user and bcrypt.check_password_hash(user.password, password):
+        log_to_console(f"User {username} logged in successfully", tag="LOGIN", spacing=1)
+        loginSuccess(username)
+        return jsonify({'message': f'Welcome, {username}!'}), 200
+    else:
+        return jsonify({'error': 'Invalid username or password'}), 401
+    
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.json
+    username = data.get('username')
+    password = data.get("password")
+
+    log_to_console(f"Received registration request for user: {username}", tag="REGISTER", spacing=1)
+
+    if not username or not password:
+        return jsonify({'error': 'Missing username or password'}), 400
+    
+    if User.query.filter_by(username=username).first():
+        return jsonify({'error': 'User already exists'}), 400
+    
+    hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+    new_user = User(username=username, password=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+
+    create_user(username, password)
+
+    log_to_console(f"User {username} created successfully", tag="REGISTER", spacing=1)
+    loginSuccess(username)
+    return jsonify({'message': f'User {username} created successfully'}), 201
+
+
+@app.route('/session-status', methods=['GET'])
+def session_status():
+
+    log_to_console(f"Checking session status for user: {session.get('username', 'None')}", tag="SESSION-STATUS", spacing=1)
+
+    if 'username' in session:
+        return jsonify({'loggedIn': True, 'user': session['username']})
+    else:
+        return jsonify({'loggedIn': False})
 
 #-----------------------------------------------------Error Handling-----------------------------------------------------#
 
@@ -79,4 +310,8 @@ def forbidden_error(error):
 #-----------------------------------------------------Main Function-----------------------------------------------------#
 
 if __name__ == '__main__':
+    if not os.path.exists(USERDATA_DIR):
+        os.makedirs(USERDATA_DIR)
+
+    init_db()
     app.run(debug=True)
--- Flask/static/scripts/script.js
+++ Flask/static/scripts/script.js
@@ -2,11 +2,27 @@ var outputDiv;
 var promptInput;
 var promptHistory = [];
 var promptHistoryIndex = 0;
-const user = 'Guest'; // The user's name (temporary)
+var user = 'Guest'; // The user's name (temporary)
+var chatID = 1; // The chat ID
+var canPrompt = false; // Whether the user can prompt the model or not
+var loggedIn = false; // Whether the user is logged in or not
+
+var loginModal;
+var loginButton;
+var registerButton;
+
+var registerModal;
+var registerButton;
+
+var accountModal;
 
 document.addEventListener("DOMContentLoaded", async function (event) {
     outputDiv = document.getElementById('output');
     promptInput = document.getElementById('prompt-input');
+
+    loginModal = document.getElementById('loginModal');
+    registerModal = document.getElementById('registerModal');
+    accountModal = document.getElementById('accountModal');
 
     promptInput.addEventListener
     // Execute the prompt when the user presses Enter
@@ -14,38 +30,33 @@ document.addEventListener("DOMContentLoaded", async function (event) {
         if (e.key === 'Enter') executePrompt();
     });
 
+    // Check if the user is logged in
+    await checkSession();
+
+    await getChatID();
+
+    // Handle login form submission
+    document.getElementById('loginForm').onsubmit = async (event) => {
+        attemptLogin(event);
+    };
+
+    // Handle register form submission
+    document.getElementById('registerForm').onsubmit = async (event) => {
+        createUser(event);
+    }
+
+    // Could remove - button up/down to retrieve previous prompts
     promptInput.addEventListener('keydown', (e) => {
-        if (e.key === 'ArrowUp') {
-            console.log('Trying to retrieve previous prompt');
-            promptHistoryIndex += 1;
-            promptHistoryIndex = Math.min(promptHistoryIndex, promptHistory.length); //
-            promptHistoryIndex = Math.max(promptHistoryIndex, 0);
-            console.log(promptHistoryIndex);
-            let previousPrompt = promptHistory[promptHistory.length - promptHistoryIndex];
-            
-            console.log(previousPrompt);
-            
+        handlePromptKeydown(e);
+    });
 
-            if (previousPrompt === undefined) return;
-            promptInput.value = previousPrompt;
-
-            // Move the cursor to the end of the input
-            promptInput.setSelectionRange(promptInput.value.length, promptInput.value.length);
-        }
-        else if (e.key === 'ArrowDown') {
-            console.log('Trying to retrieve next prompt');
-            promptHistoryIndex -= 1;
-            promptHistoryIndex = Math.max(promptHistoryIndex, 0); // Ensure the index is not negative
-            promptHistoryIndex = Math.min(promptHistoryIndex, promptHistory.length); // Ensure the index is not greater than the length of the history
-            console.log(promptHistoryIndex);
-            let nextPrompt = promptHistory[promptHistory.length - promptHistoryIndex];
-            console.log(nextPrompt);
-
-            if (nextPrompt === undefined) return;
-            promptInput.value = nextPrompt;
-
-            // Move the cursor to the end of the input
-            promptInput.setSelectionRange(promptInput.value.length, promptInput.value.length);
+    // Audio button
+    document.getElementById('output').addEventListener('click', function(event) {
+        // Check if the clicked element is the audio button
+        if (event.target && (event.target.matches('button.audio-button') || event.target.matches('img.audio-icon'))) {
+            // Access the parent div of the clicked button
+            const parentDiv = event.target.closest('.model-response-container');
+            playAudio(parentDiv);
         }
     });
 
@@ -53,8 +64,228 @@ document.addEventListener("DOMContentLoaded", async function (event) {
     promptInput.focus();
 });
 
+function openLogin() {
+    loginModal.style.display = 'block';
+}
+
+function closeLogin() {
+    loginModal.style.display = 'none';
+}
+
+function swapLoginButton() {
+    loginButton = document.getElementById('login-button');
+    userButton = document.getElementById('user-button');
+    loginButton.style.display = 'none';
+    userButton.style.display = 'block';
+}
+
+function switchToRegisterModal() {
+    loginModal.style.display = 'none';
+    registerModal.style.display = 'block';
+}
+
+function switchToLoginModal() {
+    registerModal.style.display = 'none';
+    loginModal.style.display = 'block';
+}
+
+function closeRegister() {
+    registerModal.style.display = 'none';
+}
+
+function changeUser() {
+    closeDetails();
+    openLogin();
+}
+
+function openDetails() {
+    accountModal.style.display = 'block';
+}
+
+function closeDetails() {
+    accountModal.style.display = 'none';
+}
+
+function loginSuccess(username) {
+    loggedIn = true;
+    user = username;
+
+    // Update the user's account details
+    document.getElementById('accountUsername').innerText = user;
+
+    // Update the account details button
+    document.getElementById('user-button').innerText = user;
+
+
+    swapLoginButton();
+    getChatID();
+}
+
+async function checkSession() {
+    try {
+        const response = await fetch('/session-status', {
+            method: 'GET',
+            credentials: 'include' // Include session cookies
+        });
+
+        const result = await response.json();
+
+        if (result.loggedIn) {
+            console.log(`User is logged in as: ${result.user}`);
+            loginSuccess(result.user);
+        } else {
+            console.log('User is not logged in.');
+            // UI is already set up
+        }
+    } catch (error) {
+        console.error('Error checking session status:', error);
+    }
+}
+
+async function attemptLogin(event) {
+    event.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    try {
+        const response = await fetch('/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password
+            })
+        });
+
+        const result = await response.json();
+        if (response.ok) {
+            alert(result.message);
+            loginModal.style.display = 'none'; // Close login modal on successful login
+
+            loginSuccess(username);
+        } else {
+            console.error('Error logging in:', result.error);
+            alert(result.error);
+        }
+    } catch (error) {
+        console.error('Error logging in:', error);
+        alert('An error occurred.');
+    }
+}
+
+async function createUser(event) {
+    event.preventDefault();
+    const username = document.getElementById('register-username').value;
+    const password = document.getElementById('register-password').value;
+
+    try {
+        const response = await fetch('/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: username,
+                password: password
+            })
+        });
+
+        const result = await response.json();
+        if (response.ok) {
+            alert(result.message);
+            registerModal.style.display = 'none'; // Close registration modal on successful login
+
+            loginSuccess(username);
+        } else {
+            console.error('Error creating user:', result.error);
+            alert(result.error);
+        }
+    } catch (error) {
+        console.error('Error creating user:', error);
+        alert('An error occurred.');
+    }
+}
+
+function handlePromptKeydown(e) {
+    if (e.key === 'ArrowUp') {
+        console.log('Trying to retrieve previous prompt');
+        promptHistoryIndex += 1;
+        promptHistoryIndex = Math.min(promptHistoryIndex, promptHistory.length); //
+        promptHistoryIndex = Math.max(promptHistoryIndex, 0);
+        console.log(promptHistoryIndex);
+        let previousPrompt = promptHistory[promptHistory.length - promptHistoryIndex];
+        
+        console.log(previousPrompt);
+        
+
+        if (previousPrompt === undefined) return;
+        promptInput.value = previousPrompt;
+
+        // Move the cursor to the end of the input
+        promptInput.setSelectionRange(promptInput.value.length, promptInput.value.length);
+    }
+    else if (e.key === 'ArrowDown') {
+        console.log('Trying to retrieve next prompt');
+        promptHistoryIndex -= 1;
+        promptHistoryIndex = Math.max(promptHistoryIndex, 0); // Ensure the index is not negative
+        promptHistoryIndex = Math.min(promptHistoryIndex, promptHistory.length); // Ensure the index is not greater than the length of the history
+        console.log(promptHistoryIndex);
+        let nextPrompt = promptHistory[promptHistory.length - promptHistoryIndex];
+        console.log(nextPrompt);
+
+        if (nextPrompt === undefined) return;
+        promptInput.value = nextPrompt;
+
+        // Move the cursor to the end of the input
+        promptInput.setSelectionRange(promptInput.value.length, promptInput.value.length);
+    }
+}
+
+async function getChatID() {
+    if (!loggedIn) {
+        outputDiv.innerHTML += `<p style="color: red;" class = model-error>You must be logged in to chat.</p>`;
+        return;
+    }
+    try {
+        const response = await fetch('/chat-id', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                username: user
+            })
+        });
+
+        const result = await response.json();
+
+        if (response.ok) {
+            chatID = result.chatID;
+            console.log('Chat ID:', chatID);
+            canPrompt = true;
+        } else {
+            console.error('Error getting chat ID (response failure):', result.error);
+            outputDiv.innerHTML += `<p style="color: red;" class = model-error>Error: ${result.error}</p>`;
+        }
+    } catch (error) {
+        console.error('Error getting chat ID (system error):', error);
+        outputDiv.innerHTML += `<p style="color: red;" class = model-error>Error: ${error}</p>`;
+    }
+}
 
 async function executePrompt() {
+    if (!loggedIn) {
+        alert('Please log in to prompt the model.');
+        return;
+    }
+
+    if (!canPrompt) {
+        outputDiv.innerHTML += `<p style="color: red;" class = model-error>Error: You cannot prompt the model yet. Please wait a few seconds and try again.</p>`;
+        return;
+    }
+
     const prompt = promptInput.value.trim();
     console.log('prompt entered: ' + prompt);
     if (!prompt) return;
@@ -71,7 +302,11 @@ async function executePrompt() {
     promptInput.value = '';
 
     // Display the prompt in the output
-    outputDiv.innerHTML += `<p style="color: #00ff00;" class = user-prompt>${user}> ${prompt}</p>`;
+    outputDiv.innerHTML += `
+        <div class="user-prompt-container text-container">
+            <p style="color: #00ff00;" class = user-prompt>${user}> ${prompt}</p>
+        </div>
+    `;
 
     try {
         const response = await fetch('/prompt', {
@@ -79,14 +314,23 @@ async function executePrompt() {
             headers: {
                 'Content-Type': 'application/json'
             },
-            body: JSON.stringify({ prompt })
+            body: JSON.stringify({
+                prompt: prompt
+            })
         });
 
         const result = await response.json();
 
         if (result.success) {
             if (result.reply) {
-                outputDiv.innerHTML += `<p class=model-response>david> ${result.reply}</p>`;
+                outputDiv.innerHTML += `
+                    <div class="model-response-container text-container">
+                        <p class=model-response>david> ${result.reply}</p>
+                        <button class="audio-button">
+                            <img src="${audioIconUrl}" alt="Play audio" class="audio-icon">
+                        </button>
+                    </div>
+                `;
             }
         } else {
             outputDiv.innerHTML += `<p style="color: red;" class = model-error>Error: ${result.error}</p>`;
@@ -96,6 +340,66 @@ async function executePrompt() {
     }
 
     // Scroll to the bottom and clear the input
-    outputDiv.scrollTop = outputDiv.scrollHeight;
+    window.scrollTo(0, document.body.scrollHeight);
     promptInput.value = '';
+}
+
+async function playAudio(parentDiv) {
+    stopAudio(); // This doesn't work :)
+    if (isAudioPlaying()) { // Neither does this
+        alert('An audio is already playing.');
+        return;
+    }
+
+    // Get the model response text
+    const modelResponse = parentDiv.getElementsByClassName('model-response')[0];
+    const modelResponses = document.getElementsByClassName('model-response');
+    //const modelResponses = document.getElementsByClassName('model-response');
+    //const lastModelResponse = modelResponses[modelResponses.length - 1];
+
+    // find the index of the selected model response
+    const index = Array.from(modelResponses).indexOf(modelResponse);
+
+    console.log('index: ' + index);
+    
+
+    try {
+        const response = await fetch('/generate-audio', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                text: modelResponse.innerText,
+                username: user,
+                chatID: chatID,
+                index: index,
+            })
+        });
+
+        if(!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const audioBlob = await response.blob();
+        const audioUrl = URL.createObjectURL(audioBlob);
+        const audio = new Audio(audioUrl);
+        audio.play();
+    }
+    catch (error) {
+        outputDiv.innerHTML += `<p style="color: red;" class = model-error>Error: ${error}</p>`;
+    }
+}
+
+function isAudioPlaying() {
+    const audio = document.getElementsByTagName('audio')[0];
+    return audio && !audio.paused;
+}
+
+function stopAudio() {
+    // Stop all audio elements
+    const audioElements = document.getElementsByTagName('audio');
+    for (let audio of audioElements) {
+        audio.pause();
+    }
 }
