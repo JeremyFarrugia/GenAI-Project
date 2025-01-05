cbb75ad6be6b79da69686bb8df5195b037de2573
Jeremy <jeremy.farrugia.20@um.edu.mt>
2025-01-05 18:27:51 +0100

    Cleaned and organised code, added sound generation models

--- Flask/flask-app.py
+++ Flask/flask-app.py
@@ -1,50 +1,25 @@
-from flask import Flask, render_template, send_from_directory, abort, request, jsonify, send_file, session, url_for
+from flask import Flask, render_template, send_from_directory, abort, request, jsonify, send_file, session, url_for, Response
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 import os, json
 import numpy as np
 import soundfile as sf
-from typing import Union
-
+from typing import Union, Literal
 
 from transformers import VitsModel, AutoTokenizer
 import torch
 import scipy.io.wavfile as wav
 
-tts_model = VitsModel.from_pretrained("facebook/mms-tts-eng") # https://huggingface.co/facebook/mms-tts-eng  Consider wavnet? I think it's better but it requires use of google cloud services
-tokeniser = AutoTokenizer.from_pretrained("facebook/mms-tts-eng")
-
-def generate_tts_file(text: str, output_path: str) -> None:
-    """
-    Generate a TTS audio file from the given text and save it to the output path
-    """
-    log_to_console(f"Generating TTS audio file for text: {text}", tag="GENERATE-TTS-FILE", spacing=1)
-    inputs = tokeniser(text, return_tensors="pt")
-
-    with torch.no_grad():
-        output = tts_model(**inputs).waveform.squeeze(0)  # Remove batch dimension if present
-
-    # Scale waveform to int16
-    audio_data = (output.numpy() * 32767).astype(np.int16)  # Scale to 16-bit PCM
-
-    log_to_console(f"Saving audio file to: {output_path}", tag="GENERATE-TTS-FILE", spacing=1)
-    wav.write(output_path, rate=tts_model.config.sampling_rate, data=audio_data)
-    log_to_console(f"Audio file saved successfully", tag="GENERATE-TTS-FILE", spacing=1)
-
 import torchaudio
-from audiocraft.models import AudioGen # https://github.com/facebookresearch/audiocraft/blob/main/docs/AUDIOGEN.md
+from audiocraft.models import AudioGen, MusicGen 
 from audiocraft.data.audio import audio_write
 
-#audio_model = AudioGen.get_pretrained('facebook/audiogen-medium')
-
 import sqlite3
-
 
 """
 TODO: 
 users
 - modal popups/updates instead of alerts
-- logout
 - profile?
 
 chat persistance
@@ -62,10 +37,7 @@ story creation
 images
 """
 
-AUTHENTICATION_TOKEN = 'a123' # Authentication token for the user
-
-#from TTS.api import TTS
-#tts = TTS("tts_models/en/ljspeech/tacotron2-DDC")
+AUTHENTICATION_TOKEN = 'a123' # TODO - Delete?
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 STATIC_DIR = os.path.join(SCRIPT_DIR, 'static')
@@ -73,6 +45,9 @@ USERDATA_DIR = os.path.join(STATIC_DIR, 'userdata')
 AUDIO_DIR = os.path.join(STATIC_DIR, 'audio')
 
 CLEAR_TEMP_ON_START = True # Flag to clear all temporary files on server start
+
+# NOTE: Debug mode causes the audio generation models to crash the server, but if set to false you will have to manually restart the server to see changes
+DEBUG = False
 
 #-------------------------------------------------------Server Setup-------------------------------------------------------#
 
@@ -87,6 +62,18 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(USERDATA_DIR, 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # Disable modification tracking
 db = SQLAlchemy(app) # Create a database object
 bcrypt = Bcrypt(app) # Create a bcrypt object for password hashing
+
+tts_model = VitsModel.from_pretrained("facebook/mms-tts-eng") # https://huggingface.co/facebook/mms-tts-eng  Consider wavnet? I think it's better but it requires use of google cloud services
+tokeniser = AutoTokenizer.from_pretrained("facebook/mms-tts-eng") 
+
+audio_model = None 
+music_model = None
+if not DEBUG: # Safety - See above (DEBUG flag)
+    audio_model = AudioGen.get_pretrained('facebook/audiogen-medium') # https://github.com/facebookresearch/audiocraft/blob/main/docs/AUDIOGEN.md
+    audio_model.set_generation_params(duration=5)  # Length of audio in seconds (Will be overwritten by the duration parameter in the generate_sound_file function)
+    #music_model = MusicGen.get_pretrained('facebook/musicgen-melody') # https://huggingface.co/facebook/musicgen-melody
+    music_model = MusicGen.get_pretrained('facebook/musicgen-large') # Switched away from melody since it had features that were not needed
+    music_model.set_generation_params(duration=20)
 
 # User Model
 class User(db.Model):
@@ -182,6 +169,77 @@ def debug_users() -> None:
         # Note: password is currently printed as encrypted
         log_to_console(f"User: {user.username}, Password: {user.password}", tag="DEBUG-USERS", spacing=0)
 
+def generate_tts_file(text: str, output_path: str) -> None:
+    """
+    Generate a TTS audio file from the given text and save it to the output path
+    """
+    log_to_console(f"Generating TTS audio file for text: {text}", tag="GENERATE-TTS-FILE", spacing=1)
+    inputs = tokeniser(text, return_tensors="pt")
+
+    with torch.no_grad():
+        output = tts_model(**inputs).waveform.squeeze(0)  # Remove batch dimension if present
+
+    # Scale waveform to int16
+    audio_data = (output.numpy() * 32767).astype(np.int16)  # Scale to 16-bit PCM
+
+    log_to_console(f"Saving audio file to: {output_path}", tag="GENERATE-TTS-FILE", spacing=1)
+    wav.write(output_path, rate=tts_model.config.sampling_rate, data=audio_data)
+    log_to_console(f"Audio file saved successfully", tag="GENERATE-TTS-FILE", spacing=1)
+
+def generate_sound_file(model: Literal['audio', 'music'], description: str, output_path: str, duration: int = 5) -> None:
+    """
+    Generate an audio file (sound effect or music) from the given description and save it to the output path
+    """
+
+    if DEBUG:
+        raise ValueError("Audio generation is disabled in debug mode") # Safety - See above (DEBUG flag)
+
+    if model == 'audio':
+        log_to_console(f"Generating audio file for description: {description}", tag="GENERATE-AUDIO-FILE", spacing=1)
+        audio_model.set_generation_params(duration=duration)
+        wav = audio_model.generate([description])
+        audio_write(output_path, wav.cpu(), audio_model.sample_rate, strategy="loudness", loudness_compressor=True)
+    elif model == 'music':
+        log_to_console(f"Generating music file for description: {description}", tag="GENERATE-MUSIC-FILE", spacing=1)
+        music_model.set_generation_params(duration=duration)
+        wav = music_model.generate([description])
+        audio_write(output_path, wav.cpu(), music_model.sample_rate, strategy="loudness")
+    else:
+        raise ValueError("Invalid model type")
+
+    log_to_console(f"Audio file saved successfully", tag="GENERATE-AUDIO-FILE", spacing=1)
+
+def validate_request(text: str, username: str, tag: str) -> tuple[bool, Response, int]:
+    """
+    Validate the request data
+
+    Returns:
+    - True if the request is valid, False otherwise
+    - Response object if the request is invalid
+    - Status code if the request is invalid
+    """
+    if not text:
+        log_to_console("No text provided", tag=tag, spacing=1)
+        return jsonify({'error': 'No text provided'}), 400
+    
+    # Check if user is in database
+    user = User.query.filter_by(username=username).first()
+    if not user:
+        log_to_console(f"User '{username}' not found", tag=tag, spacing=1)
+        return jsonify({'error': 'User not found'}), 404
+        
+    # Check if the request_data['username'] exists
+    if not os.path.exists(os.path.join(USERDATA_DIR, username)):
+        log_to_console(f"Username '{username}' not found", tag=tag, spacing=1)
+        return jsonify({'error': 'User not found'}), 404
+
+    output_path = os.path.join(USERDATA_DIR, username, 'temp')
+    # Create temp directory if it doesn't exist
+    if not os.path.exists(output_path):
+        os.makedirs(output_path)
+
+    return True, None, 0 # Success
+
 #-----------------------------------------------------Routes-----------------------------------------------------#
 
 # Index page
@@ -217,7 +275,8 @@ def prompt():
 def tts_request():
     try:
         # disable debug mode (debug mode was causing some issue, instead of investigating, I just disabled and re-enabled later)
-        app.debug = False
+        if DEBUG:
+            app.debug = False
 
         # Get user input (e.g., text or story context)
         request_data = {
@@ -229,19 +288,11 @@ def tts_request():
 
         log_to_console(f"Received request to generate tts: {request_data}", tag="GENERATE-TTS", spacing=1)
 
-        if not request_data['text']:
-            log_to_console("No text provided", tag="GENERATE-TTS", spacing=1)
-            return jsonify({'error': 'No text provided'}), 400
+        validate_success, response, status_code = validate_request(request_data['text'], request_data['username'], "GENERATE-TTS")
+        if not validate_success:
+            return response, status_code
         
-        # Check if the request_data['username'] exists
-        if not os.path.exists(os.path.join(USERDATA_DIR, request_data['username'])):
-            log_to_console(f"Username '{request_data['username']}' not found", tag="GENERATE-TTS", spacing=1)
-            return jsonify({'error': 'User not found'}), 404
-
-        output_path = os.path.join(USERDATA_DIR, request_data['username'], 'temp')
-        # Create temp directory if it doesn't exist
-        if not os.path.exists(output_path):
-            os.makedirs(output_path)
+        output_path = os.path.join(USERDATA_DIR, request_data['username'], 'temp')        
         output_path = os.path.join(output_path, f"{request_data['chatID']}_{request_data['index']}.wav")
 
         # Check if the audio file already exists
@@ -249,23 +300,23 @@ def tts_request():
             log_to_console(f"Audio file for message {request_data['index']} in chat {request_data['chatID']} already exists", tag="GENERATE-TTS", spacing=1)
             return send_file(output_path, mimetype='audio/wav')
 
-        #tts.tts_to_file(request_data['text'], file_path=output_path)
-
         generate_tts_file(request_data['text'], output_path)
 
         # Enable debug mode (Re-enabling might introduce the same issues but it seemed to work)
-        app.debug = True
+        if DEBUG:
+            app.debug = True
 
         # Return the audio file to the client
         return send_file(output_path, mimetype='audio/wav')
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-'''@app.route('/generate-audio', methods=['POST'])
+@app.route('/generate-audio', methods=['POST'])
 def sound_effect_request(): #TODO - change implementation to be more suitable for story ornamentation
     try:
         # disable debug mode (debug mode was causing some issue, instead of investigating, I just disabled and re-enabled later)
-        app.debug = False
+        if DEBUG:
+            app.debug = False
 
         # Get user input (e.g., text or story context)
         request_data = {
@@ -277,19 +328,11 @@ def sound_effect_request(): #TODO - change implementation to be more suitable fo
 
         log_to_console(f"Received request to generate sound effect: {request_data}", tag="GENERATE-AUDIO", spacing=1)
 
-        if not request_data['text']:
-            log_to_console("No text provided", tag="GENERATE-AUDIO", spacing=1)
-            return jsonify({'error': 'No text provided'}), 400
+        validate_success, response, status_code = validate_request(request_data['text'], request_data['username'], "GENERATE-AUDIO")
+        if not validate_success:
+            return response, status_code
         
-        # Check if the request_data['username'] exists # TODO - turn all these checks into functions
-        if not os.path.exists(os.path.join(USERDATA_DIR, request_data['username'])):
-            log_to_console(f"Username '{request_data['username']}' not found", tag="GENERATE-AUDIO", spacing=1)
-            return jsonify({'error': 'User not found'}), 404
-
         output_path = os.path.join(USERDATA_DIR, request_data['username'], 'temp')
-        # Create temp directory if it doesn't exist
-        if not os.path.exists(output_path):
-            os.makedirs(output_path)
         output_path = os.path.join(output_path, f"{request_data['chatID']}_{request_data['index']}_se.wav")
 
         # Check if the audio file already exists
@@ -298,18 +341,16 @@ def sound_effect_request(): #TODO - change implementation to be more suitable fo
             return send_file(output_path, mimetype='audio/wav')
 
         descriptions = request_data['text']
-        wav = audio_model.generate(descriptions)
-
-        # Will save with loudness normalization at -14 db LUFS.
-        audio_write(output_path, wav.cpu(), audio_model.sample_rate, strategy="loudness", loudness_compressor=True)
+        generate_sound_file('audio', descriptions, output_path, duration=5)
 
         # Enable debug mode (Re-enabling might introduce the same issues but it seemed to work)
-        app.debug = True
+        if DEBUG:
+            app.debug = True
 
         # Return
         return send_file(output_path, mimetype='audio/wav')
     except Exception as e:
-        return jsonify({'error': str(e)}), 500'''
+        return jsonify({'error': str(e)}), 500
     
 @app.route('/chat-id', methods=['POST'])
 def get_chat_id():
@@ -501,4 +542,4 @@ if __name__ == '__main__':
     with app.app_context():
         debug_users()
 
-    app.run(debug=True)
+    app.run(debug=DEBUG)
